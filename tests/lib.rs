8e29328d5df1c66afb60584ba0c1c4a135ee9c76
Galimov Arsen Gareginovich <loomaclin@yandex.ru>
2017-01-08 10:40:38 +0000

    Change test auth from `loomaclin/123` to `test/test`.

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,6 +5,6 @@ use tarantool::client::Client;
 
 #[test]
 fn tarantool_with_builder() {
-    let mut tarantool_instance = Tarantool::new("127.0.0.1:3301", "loomaclin", "123");
+    let mut tarantool_instance = Tarantool::new("127.0.0.1:3301", "test", "test");
     tarantool_instance.auth();
 }
