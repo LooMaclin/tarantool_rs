83ed375fef8791692f06e4148304d19307a92432
loo.maclin <loo.maclin@protonmail.com>
2017-02-01 00:23:11 +0500

    Update unit tests.

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,22 +7,6 @@ use tarantool::iterator_type::IteratorType;
 use std::borrow::Cow;
 use rmpv::Value;
 
-#[derive(Debug)]
-pub struct Group<'a> {
-    id: u64,
-    name: &'a str,
-    year: u64,
-}
-
-fn print_group(result: &Vec<Value>) {
-    let group = Group {
-        id: result.get(0).unwrap_or(&Value::U64(0)).as_u64().unwrap(),
-        name: result.get(1).unwrap().as_str().unwrap(),
-        year: result.get(2).unwrap_or(&Value::U64(0)).as_u64().unwrap(),
-    };
-    println!("Group: {:?}", group);
-}
-
 #[test]
 fn tarantool_with_builder() {
     let mut tarantool_instance = Tarantool::auth("127.0.0.1:3301", "test", "test").unwrap_or_else(|err| {
@@ -33,6 +17,6 @@ fn tarantool_with_builder() {
     });
     for (index, tuple) in tuples.iter().enumerate() {
         let tuple = tuple.as_array().unwrap();
-        print_group(tuple);
+        println!("{}: {:?}", index, tuple);
     }
 }
