9f0bfe9ca254f676b82da9fae922f57170c010ee
loo.maclin <loo.maclin@protonmail.com>
2017-02-17 05:55:47 +0500

    Add benchmark for select operation.

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,3 +1,6 @@
+#![feature(test)]
+extern crate test;
+
 extern crate tarantool;
 extern crate rmpv;
 extern crate futures;
@@ -32,6 +35,7 @@ fn tarantool_sync() {
 
 
 #[test]
+#[ignore]
 fn tarantool_async() {
     let mut core = Core::new().unwrap();
     let addr = "127.0.0.1:3301".parse().unwrap();
@@ -48,4 +52,16 @@ fn tarantool_async() {
             })
     ).unwrap();
 
+}
+
+#[bench]
+fn tarantool_select_benchmark(b: &mut Bencher) {
+    let mut tarantool_instance = Tarantool::auth("127.0.0.1:3301", "test", "test").unwrap_or_else(|err| {
+        panic!("Tarantool auth error: {:?}", &err);
+    });
+    b.iter(|| {
+        let tuples = tarantool_instance.select(512, 0, 10, 0, IteratorType::All, (3)).unwrap_or_else(|err| {
+            panic!("Tarantool select error: {:?}", &err);
+        });
+    });
 }
