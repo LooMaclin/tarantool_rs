5f7433556099853d2d520b8b9cb4d2f6eac8a169
loo.maclin <loo.maclin@protonmail.com>
2017-03-01 04:23:35 +0500

    Add the paste function test.

--- tests/integration_sync.rs
+++ tests/integration_sync.rs
@@ -48,6 +48,18 @@ fn tarantool_insert_select() {
     println!("Result: {:?}", result);
 }
 
+#[test]
+fn tarantool_replace_select() {
+    let mut tarantool_instance = Tarantool::auth("127.0.0.1:3301", "test", "test").unwrap_or_else(|err| {
+        panic!("Tarantool auth error: {:?}", &err);
+    });
+    let inserting_value = vec![Value::from(6), Value::String("Black Room Sign".to_string()), Value::from(2005)];
+    let result = tarantool_instance.replace(512, inserting_value).unwrap_or_else(|err| {
+        panic!("Tarantool insert error: {:?}", &err);
+    });
+    println!("Result: {:?}", result);
+}
+
 
 #[test]
 #[ignore]
