9dbde42080d54e4d6f25b82b72e8b8566d7b86a2
loo.maclin <loo.maclin@protonmail.com>
2017-03-01 06:42:22 +0500

    Add the operations enums.

---
+++ src/operation.rs
@@ -0,0 +1,19 @@
+pub enum IntegerOperation {
+    Addition = 0x2B,
+    Subtraction = 0x2D,
+    And = 0x26,
+    Xor = 0x5E,
+    Or = 0x7C,
+    Delete = 0x23,
+    Insert = 0x21,
+    Assign = 0x3D,
+}
+
+pub enum StringOperation {
+    Splice = 0x3A,
+    Delete = 0x23,
+    Insert = 0x21,
+    Assign = 0x3D,
+}
+
+pub const FIX_STR_PREFIX : u8 = 0xA1;
