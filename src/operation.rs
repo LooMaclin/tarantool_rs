88185443b02236b08558adec44c0efd0c94b2902
loo.maclin <loo.maclin@protonmail.com>
2017-03-01 15:47:34 +0500

    Split the operations enums.

--- src/operation.rs
+++ src/operation.rs
@@ -4,6 +4,9 @@ pub enum IntegerOperation {
     And = 0x26,
     Xor = 0x5E,
     Or = 0x7C,
+}
+
+pub enum CommonOperation {
     Delete = 0x23,
     Insert = 0x21,
     Assign = 0x3D,
@@ -11,9 +14,6 @@ pub enum IntegerOperation {
 
 pub enum StringOperation {
     Splice = 0x3A,
-    Delete = 0x23,
-    Insert = 0x21,
-    Assign = 0x3D,
 }
 
 pub const FIX_STR_PREFIX : u8 = 0xA1;
