300fc029c08929269e03d22281deffdb967b1d27
loo.maclin <loo.maclin@protonmail.com>
2017-03-02 08:10:17 +0500

    Add the integration test of delete function.

--- tests/integration_sync.rs
+++ tests/integration_sync.rs
@@ -93,6 +93,18 @@ fn tarantool_sync_update_common() {
     });
 }
 
+#[test]
+fn tarantool_sync_delete() {
+    let mut tarantool_instance = Tarantool::auth("127.0.0.1:3301", "test", "test").unwrap_or_else(|err| {
+        panic!("Tarantool auth error: {:?}", &err);
+    });
+    let delete_key = vec![Value::from(12)];
+    let result = tarantool_instance.delete(512, 0, delete_key).unwrap_or_else(|err| {
+        panic!("Tarantool delete error: {:?}", &err);
+    });
+    println!("Result: {:?}", result);
+}
+
 
 
 #[test]
