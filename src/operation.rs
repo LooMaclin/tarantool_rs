056c01a2dc36f397b5416b0eeb9742838f16d249
loo.maclin <loo.maclin@protonmail.com>
2017-03-02 09:01:17 +0500

    Add the upsert operations enum.

--- src/operation.rs
+++ src/operation.rs
@@ -16,4 +16,12 @@ pub enum StringOperation {
     Splice = 0x3A,
 }
 
+pub enum UpsertOperation {
+    Add = 0x2B,
+    Subtract = 0x2D,
+    Assign = 0x3D,
+    Insert = 0x21,
+    Delete = 0x23,
+}
+
 pub const FIX_STR_PREFIX : u8 = 0xA1;
