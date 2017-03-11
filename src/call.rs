6140e8e930d643b3189fb694e835891c7df268ec
loo.maclin <loo.maclin@protonmail.com>
2017-03-11 10:21:22 +0500

    Update call for new API.

--- src/call.rs
+++ src/call.rs
@@ -5,23 +5,23 @@ use byteorder::BigEndian;
 use request_type_key::RequestTypeKey;
 use code::Code;
 use serde::Serialize;
+use tarantool::Tarantool;
 
 #[derive(Debug, Builder)]
 pub struct Call<'a> {
-    id: u32,
     function_name: &'static str,
     keys: &'a Vec<Value>,
 }
 
 impl<'a> Call<'a> {
-    pub fn perform<I>(&self)
+    pub fn perform<I>(&self, state: &mut Tarantool)
                       -> Result<Value, String>
         where I: Serialize
     {
-        let wrapped_keys = Value::Array(keys);
+        let wrapped_keys = Value::Array(self.keys);
         let keys_buffer = serialize_keys(wrapped_keys);
-        let function_name = serialize_keys(Value::String(function_name.into()));
-        let request_id = self.get_id();
+        let function_name = serialize_keys(Value::String(self.function_name.into()));
+        let request_id = state.get_id();
         let header = header(RequestTypeKey::Call, request_id);
         let mut body = [&[0x82][..],
             &[Code::FunctionName as u8][..],
@@ -29,7 +29,7 @@ impl<'a> Call<'a> {
             &[Code::Tuple as u8][..],
             &keys_buffer[..]]
             .concat();
-        let response = request(&header, &body);
+        let response = request(&header, &body, &mut state.descriptor);
         process_response(&response)
     }
 }
