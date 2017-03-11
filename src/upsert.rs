d4636e56bbfbfa6274d626e2d90e54f18df0ed33
loo.maclin <loo.maclin@protonmail.com>
2017-03-11 10:06:44 +0500

    Update upsert for new API.

--- src/upsert.rs
+++ src/upsert.rs
@@ -5,10 +5,12 @@ use byteorder::BigEndian;
 use request_type_key::RequestTypeKey;
 use code::Code;
 use serde::Serialize;
+use upsert_operation::UpsertOperation;
+use tarantool::Tarantool;
+use FIX_STR_PREFIX;
 
 #[derive(Debug, Builder)]
 pub struct Upsert<'a> {
-    id: u32,
     space: u16,
     keys: &'a Vec<Value>,
     operation_type: UpsertOperation,
@@ -17,27 +19,26 @@ pub struct Upsert<'a> {
 }
 
 impl<'a> Upsert<'a> {
-    pub fn perform<I>(&self)
+    pub fn perform<I>(&self, state: &mut Tarantool)
                       -> Result<Value, String>
         where I: Serialize
     {
-        let keys_buffer = serialize_keys(keys);
-        let request_id = self.get_id();
+        let keys_buffer = serialize_keys(self.keys);
+        let request_id = state.get_id();
         let header = header(RequestTypeKey::Update, request_id);
-        let wrapped_argument = Value::from(argument);
-        let mut serialized_argument = Vec::new();
-        wrapped_argument.serialize(&mut Serializer::new(&mut serialized_argument)).unwrap();
+        let wrapped_argument = Value::from(self.argument);
+        let mut serialized_argument = serialize_keys(wrapped_argument);
         let mut body = [&[0x84][..],
             &[Code::SpaceId as u8][..],
             &[0xCD, 0x0, 0x0][..],
             &[Code::Key as u8][..],
             &keys_buffer[..],
             &[Code::Tuple as u8][..],
-            &[0x91, 0x93, FIX_STR_PREFIX, operation_type as u8, field_number][..],
+            &[0x91, 0x93, FIX_STR_PREFIX, self.operation_type as u8, self.field_number][..],
             &serialized_argument[..]]
             .concat();
-        BigEndian::write_u16(&mut body[3..5], space);
-        let response = request(&header, &body);
+        BigEndian::write_u16(&mut body[3..5], self.space);
+        let response = request(&header, &body, &mut state.descriptor);
         process_response(&response)
     }
 }
