a625cc6803a9cba2bd50661d1ed659a64d304859
loo.maclin <loo.maclin@protonmail.com>
2017-03-11 10:16:42 +0500

    Update update_string for new API.

--- src/update_string.rs
+++ src/update_string.rs
@@ -5,10 +5,13 @@ use byteorder::BigEndian;
 use request_type_key::RequestTypeKey;
 use code::Code;
 use serde::Serialize;
+use tarantool::Tarantool;
+use FIX_STR_PREFIX;
+use string_operation::StringOperation;
+use std::borrow::Cow;
 
 #[derive(Debug, Builder)]
 pub struct UpdateString<'a> {
-    id: u32,
     space: u16,
     index: u8,
     field_number: u8,
@@ -19,21 +22,20 @@ pub struct UpdateString<'a> {
 }
 
 impl<'a> UpdateString<'a> {
-    pub fn perform<I>(&self)
+    pub fn perform<I>(&self, state: &mut Tarantool)
                       -> Result<Value, String>
         where I: Serialize
     {
-        let keys_buffer = serialize_keys(keys);
-        let request_id = self.get_id();
+        let keys_buffer = serialize_keys(self.keys);
+        let request_id = state.get_id();
         let header = header(RequestTypeKey::Update, request_id);
-        let wrapped_argument = Value::String(argument.into().into_owned());
-        let mut serialized_argument = Vec::new();
-        wrapped_argument.serialize(&mut Serializer::new(&mut serialized_argument)).unwrap();
+        let wrapped_argument = Value::String(self.argument.into().into_owned());
+        let mut serialized_argument = serialize_keys(wrapped_argument);
         let mut body = [&[0x84][..],
             &[Code::SpaceId as u8][..],
             &[0xCD, 0x0, 0x0][..],
             &[Code::IndexId as u8][..],
-            &[index][..],
+            &[self.index][..],
             &[Code::Key as u8][..],
             &keys_buffer[..],
             &[Code::Tuple as u8][..],
@@ -41,13 +43,13 @@ impl<'a> UpdateString<'a> {
                 0x95,
                 FIX_STR_PREFIX,
                 StringOperation::Splice as u8,
-                field_number,
-                position,
-                offset][..],
+                self.field_number,
+                self.position,
+                self.offset][..],
             &serialized_argument[..]]
             .concat();
-        BigEndian::write_u16(&mut body[3..5], space);
-        let response = request(&header, &body);
+        BigEndian::write_u16(&mut body[3..5], self.space);
+        let response = request(&header, &body, &mut state.descriptor);
         process_response(&response)
     }
 }
