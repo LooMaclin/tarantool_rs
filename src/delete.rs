22ca2cdc220fa67c4d7f65ad30e6c6b4c992b7ed
loo.maclin <loo.maclin@protonmail.com>
2017-03-11 10:24:14 +0500

    Update delete for new API.

--- src/delete.rs
+++ src/delete.rs
@@ -5,34 +5,34 @@ use byteorder::BigEndian;
 use request_type_key::RequestTypeKey;
 use code::Code;
 use serde::Serialize;
+use tarantool::Tarantool;
 
 #[derive(Debug, Builder)]
 pub struct Delete<'a> {
-    id: u32,
     space: u16,
     index: u8,
     keys: &'a Vec<Value>,
 }
 
 impl<'a> Delete<'a> {
-    pub fn perform<I>(&self)
+    pub fn perform<I>(&self, state: &mut Tarantool)
                       -> Result<Value, String>
         where I: Serialize
     {
-        let wrapped_keys = Value::Array(keys);
+        let wrapped_keys = Value::Array(self.keys);
         let keys_buffer = serialize_keys(wrapped_keys);
-        let request_id = self.get_id();
+        let request_id = state.get_id();
         let header = header(RequestTypeKey::Delete, request_id);
         let mut body = [&[0x83][..],
             &[Code::SpaceId as u8][..],
             &[0xCD, 0x0, 0x0][..],
             &[Code::IndexId as u8][..],
-            &[index][..],
+            &[self.index][..],
             &[Code::Key as u8][..],
             &keys_buffer[..]]
             .concat();
-        BigEndian::write_u16(&mut body[3..5], space);
-        let response = request(&header, &body);
+        BigEndian::write_u16(&mut body[3..5], self.space);
+        let response = request(&header, &body, &mut state.descriptor);
         process_response(&response)
     }
 }
