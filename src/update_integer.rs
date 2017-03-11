d5bef8026030816eef7d688c7676e1f1136969b6
loo.maclin <loo.maclin@protonmail.com>
2017-03-11 10:11:23 +0500

    Update update_integer for new API.

--- src/update_integer.rs
+++ src/update_integer.rs
@@ -5,6 +5,9 @@ use byteorder::BigEndian;
 use request_type_key::RequestTypeKey;
 use code::Code;
 use serde::Serialize;
+use integer_operation::IntegerOperation;
+use FIX_STR_PREFIX;
+use tarantool::Tarantool;
 
 #[derive(Debug, Builder)]
 pub struct UpdateInteger<'a> {
@@ -18,29 +21,28 @@ pub struct UpdateInteger<'a> {
 }
 
 impl<'a> UpdateInteger<'a> {
-    pub fn perform<I>(&self)
+    pub fn perform<I>(&self, state: &mut Tarantool)
                       -> Result<Value, String>
         where I: Serialize
     {
-        let keys_buffer = serialize_keys(keys);
-        let request_id = self.get_id();
+        let keys_buffer = serialize_keys(self.keys);
+        let request_id = state.get_id();
         let header = header(RequestTypeKey::Update, request_id);
-        let wrapped_argument = Value::from(argument);
-        let mut serialized_argument = Vec::new();
-        wrapped_argument.serialize(&mut Serializer::new(&mut serialized_argument)).unwrap();
+        let wrapped_argument = Value::from(self.argument);
+        let mut serialized_argument = serialize_keys(wrapped_argument);
         let mut body = [&[0x84][..],
             &[Code::SpaceId as u8][..],
             &[0xCD, 0x0, 0x0][..],
             &[Code::IndexId as u8][..],
-            &[index][..],
+            &[self.index][..],
             &[Code::Key as u8][..],
             &keys_buffer[..],
             &[Code::Tuple as u8][..],
-            &[0x91, 0x93, FIX_STR_PREFIX, operation_type as u8, field_number][..],
+            &[0x91, 0x93, FIX_STR_PREFIX, self.operation_type as u8, self.field_number][..],
             &serialized_argument[..]]
             .concat();
-        BigEndian::write_u16(&mut body[3..5], space);
-        let response = request(&header, &body);
+        BigEndian::write_u16(&mut body[3..5], self.space);
+        let response = request(&header, &body, &mut state.descriptor);
         process_response(&response)
     }
 }
