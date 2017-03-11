539d024fa4d451872451ec5451ea57cce6cd54f3
loo.maclin <loo.maclin@protonmail.com>
2017-03-11 10:26:07 +0500

    Update replace for new API.

--- src/replace.rs
+++ src/replace.rs
@@ -5,26 +5,22 @@ use byteorder::BigEndian;
 use request_type_key::RequestTypeKey;
 use code::Code;
 use serde::Serialize;
+use tarantool::Tarantool;
 
 #[derive(Debug, Builder)]
 pub struct Replace<'a> {
-    id: u32,
     space: u16,
     keys: &'a Vec<Value>,
 }
 
 impl<'a> Replace<'a> {
-    pub fn perform<I>(&self)
+    pub fn perform<I>(&self, state: &mut Tarantool)
                       -> Result<Value, String>
         where I: Serialize
     {
         let mut keys_buffer = Vec::new();
-        let wrapped_keys = Value::Array(keys);
-        wrapped_keys.serialize(&mut Serializer::new(&mut keys_buffer)).unwrap();
-        if keys_buffer.len() == 1 {
-            keys_buffer = [&[0x91][..], &keys_buffer[..]].concat();
-        }
-        let request_id = self.get_id();
+        let wrapped_keys = serialize_keys(Value::Array(self.keys));
+        let request_id = state.get_id();
         let header = header(RequestTypeKey::Replace, request_id);
         let mut body = [&[0x82][..],
             &[Code::SpaceId as u8][..],
@@ -32,8 +28,8 @@ impl<'a> Replace<'a> {
             &[Code::Tuple as u8][..],
             &keys_buffer[..]]
             .concat();
-        BigEndian::write_u16(&mut body[3..5], space);
-        let response = request(&header, &body);
+        BigEndian::write_u16(&mut body[3..5], self.space);
+        let response = request(&header, &body, &mut state.descriptor);
         process_response(&response)
     }
 }
