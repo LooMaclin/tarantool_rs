01684b49adb666c883a44eae0b4bdbf24af6f1fa
agalimov@it.ru <agalimov@it.ru>
2017-03-13 20:10:50 +0500

    Update example with new API.

--- examples/example.rs
+++ examples/example.rs
@@ -5,53 +5,72 @@ use tarantool::{Value, Tarantool, IteratorType, Select, Insert, Replace, Delete,
                 UpsertOperation};
 
 fn main() {
+
     let mut tarantool_instance = Tarantool::auth("127.0.0.1:3301", "test", "test").unwrap_or_else(|err| {
         panic!("err: {}", err);
     });
+
     let error_handler = |err| panic!("Tarantool error: {}", err);
-    let tuples = tarantool_instance.request(Select {
+
+    let select = Select {
         space: 512,
         index: 0,
         limit: 100,
         offset: 0,
         iterator: IteratorType::All,
         keys: &vec![]
-    })
-        .unwrap_or_else(&error_handler);
+    };
+
+    let tuples = tarantool_instance.request(&select).unwrap_or_else(&error_handler);
+
     println!("Select result: ");
     for (index, tuple) in tuples.as_array().unwrap().iter().enumerate() {
         let tuple = tuple.as_array().unwrap();
         println!("{}: {:?}", index, tuple);
     }
-    println!("Replace result: {:?}", tarantool_instance.request(Replace {
+
+    let replace = Replace {
         space: 512,
         keys: &vec![Value::from(1), Value::String(String::from("TEST REPLACE"))]
-    })
-        .unwrap_or_else(&error_handler));
-    println!("Delete result: {:?}", tarantool_instance.request(Delete {
+    };
+
+    println!("Replace result: {:?}", tarantool_instance.request(&replace).unwrap_or_else(&error_handler));
+
+    let delete = Delete {
         space: 512,
         index: 0,
         keys: &vec![Value::from(3)]
-    })
-        .unwrap_or_else(&error_handler));
-    println!("Common-Update result: {:?}", tarantool_instance.request(UpdateCommon {
+    };
+
+    println!("Delete result: {:?}", tarantool_instance.request(&delete).unwrap_or_else(&error_handler));
+
+    let update_common = UpdateCommon {
         space: 512,
         index: 0,
         operation_type: CommonOperation::Assign,
         field_number: 3,
         argument: Value::String(String::from("Test Update Common Assign")),
         keys: &vec![Value::from(6)]
-    })
-        .unwrap_or_else(&error_handler));
-    println!("Call result: {:?}", tarantool_instance.request(Call {
+    };
+
+    println!("Common-Update result: {:?}", tarantool_instance.request(&update_common).unwrap_or_else(&error_handler));
+
+
+    let call = Call {
         function_name: "test",
         keys: &vec![]
-    }).unwrap_or_else(&error_handler));
-    println!("Eval result: {:?}", tarantool_instance.request(Eval {
+    };
+
+    println!("Call result: {:?}", tarantool_instance.request(&call).unwrap_or_else(&error_handler));
+
+    let eval = Eval {
         expression: r#"return 5+5"#,
         keys: &vec![]
-    }).unwrap_or_else(&error_handler));
-    println!("String-Update result: {:?}", tarantool_instance.request(UpdateString {
+    };
+
+    println!("Eval result: {:?}", tarantool_instance.request(&eval).unwrap_or_else(&error_handler));
+
+    let update_string = UpdateString {
         space: 512,
         index: 0,
         field_number: 1,
@@ -59,26 +78,35 @@ fn main() {
         offset: 3,
         argument: "TEST UPDATE STRING".into(),
         keys: &vec![Value::from(2)]
-    }).unwrap_or_else(&error_handler));
-    println!("Integer-Update result: {:?}", tarantool_instance.request(UpdateInteger {
+    };
+
+    println!("String-Update result: {:?}", tarantool_instance.request(&update_string).unwrap_or_else(&error_handler));
+
+    let update_integer = UpdateInteger {
         space: 512,
         index: 0,
         operation_type: IntegerOperation::Addition,
         field_number: 2,
         argument: 1,
         keys: &vec![Value::from(4)]
-    }).unwrap_or_else(&error_handler));
-    println!("Upsert result: {:?}", tarantool_instance.request(Upsert {
+    };
+
+    println!("Integer-Update result: {:?}", tarantool_instance.request(&update_integer).unwrap_or_else(&error_handler));
+
+    let upsert = Upsert {
         space: 512,
         keys: &vec![Value::from(5)],
         operation_type: UpsertOperation::Add,
         field_number: 2,
         argument: 2,
-    }).unwrap_or_else(&error_handler));
-    println!("Insert result: {:?}",
-    tarantool_instance.request(Insert {
+    };
+
+    println!("Upsert result: {:?}", tarantool_instance.request(&upsert).unwrap_or_else(&error_handler));
+
+    let insert = Insert {
         space: 512,
         keys: &vec![Value::from(9)]
-    })
-        .unwrap_or_else(&error_handler));
+    };
+
+    println!("Insert result: {:?}", tarantool_instance.request(&insert).unwrap_or_else(&error_handler));
 }
