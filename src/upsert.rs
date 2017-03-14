87db864df9033f0770002916b13e18a06b1f9074
loo.maclin <loo.maclin@protonmail.com>
2017-03-14 07:42:01 +0500

    Refactor upsert function.

--- src/upsert.rs
+++ src/upsert.rs
@@ -24,32 +24,16 @@ pub struct Upsert<'a> {
 
 impl<'a> Action for Upsert<'a> {
     fn get(&self) -> (RequestTypeKey, Vec<u8>) {
-        let body = Value::Map(
-          vec![
-              (Value::Integer((Code::SpaceId as u64).into()), Value::Integer(self.space.into())),
-              (Value::Integer((Code::Tuple as u64).into()), Value::Array(self.keys.clone())),
-              (Value::Integer((Code::OPS as u64).into()), Value::Array(
-                      vec![
-                          read_value(&mut &[&[FIX_STR_PREFIX][..], &[self.operation_type as u8][..]].concat()[..]).unwrap(),
-                          Value::from(self.field_number),
-                          Value::from(self.argument)
-                  ]))
-          ]);
-        let body = serialize(body);
-//        let keys_buffer = serialize(self.keys);
-//        let wrapped_argument = Value::from(self.argument);
-//        let mut serialized_argument = serialize(wrapped_argument);
-//        let mut body =
-//            [&[0x84][..],
-//             &[Code::SpaceId as u8][..],
-//             &[0xCD, 0x0, 0x0][..],
-//             &[Code::Tuple as u8][..],
-//             &keys_buffer[..],
-//             &[Code::OPS as u8][..],
-//             &[0x91, 0x93, FIX_STR_PREFIX, self.operation_type as u8, self.field_number][..],
-//             &serialized_argument[..]]
-//                .concat();
-//        BigEndian::write_u16(&mut body[3..5], self.space);
-        (RequestTypeKey::Upsert, body)
+        (RequestTypeKey::Upsert, serialize(Value::Map(
+            vec![
+                (Value::from(Code::SpaceId as u8), Value::from(self.space)),
+                (Value::from(Code::Tuple as u8), Value::from(self.keys.clone())),
+                (Value::from(Code::OPS as u8), Value::Array(
+                    vec![
+                        read_value(&mut &[&[FIX_STR_PREFIX][..], &[self.operation_type as u8][..]].concat()[..]).unwrap(),
+                        Value::from(self.field_number),
+                        Value::from(self.argument)
+                    ]))
+            ])))
     }
 }
