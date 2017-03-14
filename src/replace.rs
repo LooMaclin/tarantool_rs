704f8fb14b0e661f7552a25a77624e54710b6a06
loo.maclin <loo.maclin@protonmail.com>
2017-03-14 07:17:02 +0500

    Refactor replace function.

--- src/replace.rs
+++ src/replace.rs
@@ -17,20 +17,10 @@ pub struct Replace<'a> {
 
 impl<'a> Action for Replace<'a> {
     fn get(&self) -> (RequestTypeKey, Vec<u8>) {
-        let body = Value::Map(
+        (RequestTypeKey::Replace, serialize(Value::Map(
             vec![
-                (Value::Integer((Code::SpaceId as u64).into()), Value::Integer(self.space.into())),
-                (Value::Integer((Code::Tuple as u64).into()), Value::from(self.keys.clone()))
-            ]);
-        let body = serialize(body);
-//        let keys_buffer = serialize(Value::Array(self.keys.clone()));
-//        let mut body = [&[0x82][..],
-//                        &[Code::SpaceId as u8][..],
-//                        &[0xCD, 0x0, 0x0][..],
-//                        &[Code::Tuple as u8][..],
-//                        &keys_buffer[..]]
-//            .concat();
-//        BigEndian::write_u16(&mut body[3..5], self.space);
-        (RequestTypeKey::Replace, body)
+                (Value::from(Code::SpaceId as u8), Value::from(self.space)),
+                (Value::from(Code::Tuple as u8), Value::from(self.keys.clone()))
+            ])))
     }
 }
