fb16f4450dc7e6056ad423cba5dba669e764f136
loo.maclin <loo.maclin@protonmail.com>
2017-03-21 05:14:07 +0500

    =(

--- src/validate.rs
+++ src/validate.rs
@@ -9,7 +9,7 @@ pub struct Validate<T, K> {
 }
 
 impl<T, K> Service for Validate<T,K>
-    where T: Service<Request = Vec<u8>, Response = Result<Value, Utf8String>, Error = io::Error>,
+    where T: Service<Request = K, Response = Result<Value, Utf8String>, Error = io::Error>,
           T::Future: 'static,
          K: Action
 {
@@ -18,18 +18,18 @@ impl<T, K> Service for Validate<T,K>
     type Error = io::Error;
     type Future = Box<Future<Item = Result<Value, Utf8String>, Error = io::Error>>;
 
-    fn call<K>(&self, req: K) -> Self::Future where K: Action {
+    fn call(&self, req: Self::Response) -> Self::Future {
         Box::new(self.inner.call(req).and_then(|resp| Ok(resp)))
     }
 }
 
 impl<T, K> NewService for Validate<T, K>
-    where T: NewService<Request = Vec<u8>, Response = Result<Value, Utf8String>, Error = io::Error>,
+    where T: NewService<Request = K, Response = Result<Value, Utf8String>, Error = io::Error>,
           K: Action,
-          <T::Instance as Service>::Future: 'static,
+<T::Instance as Service>::Future: 'static,
 
 {
-    type Request = Vec<u8>;
+    type Request = K;
     type Response = Result<Value, Utf8String>;
     type Error = io::Error;
     type Instance = Validate<T::Instance, K>;
