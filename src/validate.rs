127980e2efa65694c7086f5ddfd54c976f622caf
loo.maclin@protonmail.com <loo.maclin@protonmail.com>
2017-05-10 00:30:48 +0500

    remove newservice

--- src/validate.rs
+++ src/validate.rs
@@ -1,6 +1,6 @@
 use std::io;
 use futures::Future;
-use tokio_service::{Service, NewService};
+use tokio_service::{Service};
 use action_type::ActionType;
 use async_response::AsyncResponse;
 
@@ -20,20 +20,5 @@ impl<S> Service for Validate<S>
 
     fn call(&self, req: ActionType) -> Self::Future {
         Box::new(self.inner.call(req).and_then(|resp| Ok(resp)))
-    }
-}
-
-impl<S> NewService for Validate<S>
-    where S: NewService<Request = ActionType, Response = AsyncResponse, Error = io::Error>,
-          <S::Instance as Service>::Future: 'static
-{
-    type Request = ActionType;
-    type Response = AsyncResponse;
-    type Error = io::Error;
-    type Instance = Validate<S::Instance>;
-
-    fn new_service(&self) -> io::Result<Self::Instance> {
-        let inner = try!(self.inner.new_service());
-        Ok(Validate { inner: inner })
     }
 }
